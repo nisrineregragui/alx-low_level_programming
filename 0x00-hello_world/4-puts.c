259ceb76dd750ef32ac45cc60b3f71da5920c122
nisrine regragui <nisrineregragui10@gmail.com>
2023-08-10 15:49:57 -0700

    4v3

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #include<stdio.h>
-/* a comment to describe so we wanna print a text than return the 0 value */
+/**
+  *a comment to describe so we wanna print a text
+  *than return the 0 value
+ */
 int main(void)
 {puts("\"Programming is like building a multilingual puzzle");
 return (0); }
