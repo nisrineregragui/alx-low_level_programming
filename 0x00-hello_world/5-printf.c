47a14e7ab3bc15de54fb255ac1fd10be49502053
nisrine regragui <nisrineregragui10@gmail.com>
2023-08-10 16:20:32 -0700

    k

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
-/* a description for the function main*/ int main(void){
-printf("with proper grammar, but the outcome is a piece of art,\n"); 
+/* a description for the function main*/
+int main(void)
+{ printf("with proper grammar, but the outcome is a piece of art,\n"); 
 return (0); }
