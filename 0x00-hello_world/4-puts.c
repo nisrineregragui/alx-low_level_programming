f5bd87ee2ceb8c371cc1aa90c7a4903a30b38209
nisrine regragui <nisrineregragui10@gmail.com>
2023-08-10 16:10:25 -0700

    l

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,5 @@
 #include<stdio.h>
-/* main - the entry point*/
-int main(void)
-/* main - the entry point*/
+/* main - the entry point*/int main(void)/* main : the entry point*/
 { puts("\"Programming is like building a multilingual puzzle");
 /* return : always return with 0 in int */
 	return (0); }
