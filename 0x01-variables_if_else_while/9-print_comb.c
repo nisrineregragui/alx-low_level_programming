fd348209b27a0a3ce6e9423b6359bcada6934031
regragui nisrine <nisrineregragui10@gmail.com>
2023-08-11 10:38:53 -0700

    09

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/*betty*/ int main(void) /*style*/
+{ 
+int i, num =0;
+for (i = 0; i < 10 ; i++)
+{
+putchar(num + '0');
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
