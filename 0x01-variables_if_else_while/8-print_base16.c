7a5560005fceb142142df90cbe09c0dcbcf59f77
regragui nisrine <nisrineregragui10@gmail.com>
2023-08-11 10:24:03 -0700

    08

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/*comment*/ int main(void) /*comment*/
+{
+int i;
+char lettre = 'a';
+for (i = 0; i < 10 ;i++
+{putchar(i + '0');
+}
+for (i = 0; i < 6 ; i++)
+{
+putchar(lettre);
+lettre++;
+}
+putchar('\n');
+return (0);
+}
