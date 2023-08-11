565418bc73e9e08237579b36dfaeb9b72b77cdda
regragui nisrine <nisrineregragui10@gmail.com>
2023-08-11 09:32:06 -0700

    05

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-/*comment*/ int main (void) /*cpmment*/
+/*comment*/ int main(void) /*cpmment*/
 {
 int i;
 
 for (i = 0; i < 10 ; i++)
-{ 
+{
 printf("%d", i);
 }
 printf("\n");
