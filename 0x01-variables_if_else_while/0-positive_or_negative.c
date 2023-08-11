bf5ed2312f9298854a12807b031ca7bf30284cf9
nisrine regragui <nisrineregragui10@gmail.com>
2023-08-11 00:16:13 -0700

    00

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int n; 
+/*comment*/ int main (void){ /*comment*/
+if (n>0)
+printf("%d is positive", n); 
+else if (n==0)
+printf("%d is zero", n);
+else 
+printf("%d is negative", n);
+return(0);
+}
