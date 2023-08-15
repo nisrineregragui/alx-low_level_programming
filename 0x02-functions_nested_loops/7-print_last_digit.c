eb79661693d3ceeb00315c92b483097b5f0ca7c7
regragui nisrine <nisrineregragui10@gmail.com>
2023-08-15 06:56:22 -0700

    07

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,7 +2,15 @@
 /*comment*/ int print_last_digit(int); /*comment*/
 /*comment*/ int print_last_digit(int k) /*comment*/
 {
-int n ;
+int n;
+if (k < 0)
+{
+n = -k;
+n = n % 10;
+}
+else
+{
 n = k % 10;
+}
 return (n);
 }
