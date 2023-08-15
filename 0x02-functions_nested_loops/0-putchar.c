f8cdc0f6e9cc142784aee09c9faec912a05e3ec3
regragui nisrine <nisrineregragui10@gmail.com>
2023-08-15 05:26:23 -0700

    00

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/*cc*/ int main(void) /*d*/
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
