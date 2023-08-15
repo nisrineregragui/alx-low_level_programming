55d98a59d9d8bec1a2702452709cd54be8c388da
regragui nisrine <nisrineregragui10@gmail.com>
2023-08-15 04:05:44 -0700

    h

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/*comment*/ void jack_bauer(void); /*comment*/
+/*comment*/ void jack_bauer(void) /*comment*/
+{
+int h, s;
+for (h = 0;  h < 24; h++)
+{
+for (s = 0; s < 60; s++)
+{
+if (h < 10)
+{
+_putchar(0 + '0');
+}
+_putchar(h + '0');
+_putchar(':');
+if (s < 10)
+{
+_putchar(0 + '0');
+}
+_putchar(s + '0');
+}
+_putchar('\n');
+}
