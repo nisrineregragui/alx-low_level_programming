00009e77d71060877325db56f3384310d670bb40
regragui nisrine <nisrineregragui10@gmail.com>
2023-08-15 04:19:55 -0700

    hs

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,20 +2,23 @@
 /*comment*/ void jack_bauer(void); /*comment*/
 /*comment*/ void jack_bauer(void) /*comment*/
 {
-int h, s;
+int h, s, j;
 for (h = 0;  h < 24; h++)
 {
+j = 0;
 for (s = 0; s < 60; s++)
 {
-if (h < 10)
+if (h < 10 && j == 0)
 {
 _putchar(0 + '0');
+j++;
 }
 _putchar(h + '0');
 _putchar(':');
-if (s < 10)
+if (s < 10 && j == 0)
 {
 _putchar(0 + '0');
+j++;
 }
 _putchar(s + '0');
 }
