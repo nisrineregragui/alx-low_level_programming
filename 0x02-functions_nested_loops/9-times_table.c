a550816356631b571ea44b26aecead29d10ff9fb
regragui nisrine <nisrineregragui10@gmail.com>
2023-08-15 06:46:55 -0700

    09

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,15 +1,24 @@
 #include "main.h"
-/*table*/ void times_table(void) /*table*/
+/*g*/void times_table(void)/*s*/
 {
-int r, i, j;
+int r, i, j, spaces;
 for (i = 0; i < 10; i++)
 {
 for (j = 0; j < 10; j++)
 {
 r = i * j;
-_putchar(r + '0');
-_putchar(';');
+spaces = (j == 0) ? 0 : 3 - (r < 10) - (r >= 100);
+while (numSpaces > 0)
+{
 _putchar(' ');
+spaces--;
+}
+_putchar(r + '0');
+if (j < 9)
+{
+_putchar(',');
+_putchar(' ');
+}
 }
 _putchar('\n');
 }
