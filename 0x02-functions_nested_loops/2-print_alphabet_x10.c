53e5ffbe880f59593533ff9660fb0a9f1941b990
regragui nisrine <nisrineregragui10@gmail.com>
2023-08-15 01:16:48 -0700

    02

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/*somrthing*/ void print_alphabet_x10(void) /*k*/
+{
+int i, j;
+char alpha = 'a';
+for (i = 0; i < 10; i++)
+{
+for (j = 0; j < 26; j++)
+{
+_putchar(alpha);
+alpha++;
+}
+_putchar('\n');
+}
+}
