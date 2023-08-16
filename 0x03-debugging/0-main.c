b94512f2f70dbbe54b1663f352a9ace4a40f12ae
regragui nisrine <nisrineregragui10@gmail.com>
2023-08-15 22:07:14 -0700

    00

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,16 @@
 #include "main.h"
-/*betty*/int main(void)/*betty*/
+
+/**
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
+int main(void)
 {
-int i;
-i = 0;
-positive_or_negative(i);
-return (0);
+        int i;
+
+        i = 0;
+        positive_or_negative(i);
+
+        return (0);
 }
