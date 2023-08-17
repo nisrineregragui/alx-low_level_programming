3c623aa6870430036e1d75bf497061cd0c81019d
regragui nisrine <nisrineregragui10@gmail.com>
2023-08-17 03:52:36 -0700

    rr

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -4,21 +4,17 @@
 int i;
 for (i = 1; i <= 100 ; i++)
 {
-if (i % 3 == 0 && i % 5 != 0)
-{
-printf("Fizz ");
-}
-else if (i % 5 == 0 && i % 3 != 0)
-{
-printf("Buzz ");
-}
-else if (i % 5 == 0 && i % 3 == 0)
+if (i % 3 == 0 && i % 5 == 0)
 {
 printf("FizzBuzz ");
 }
-else if ( i == 1)
+else if (i % 3 == 0)
 {
-printf("%d ", i);
+printf("Fizz ");
+}
+else if (i % 5 == 0)
+{
+printf("Buzz ");
 }
 else
 {
@@ -27,4 +23,4 @@ printf("%d ", i);
 }
 printf("\n");
 return (0);
-}
+}  
