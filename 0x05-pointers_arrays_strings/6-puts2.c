b6e50f07489338466fe14828051a338a99ab2aa8
regragui nisrine <nisrineregragui10@gmail.com>
2023-08-21 13:41:43 -0700

    up

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,10 +1,23 @@
 #include "main.h"
 /*betty*/ void puts2(char *str) /*style*/
 {
-while (*str != '\0')
+int l = 0, t, i;
+char *p = str;
+while (*p != '\0')
+{
+p++;
+l++;
+}
+t = l - 1;
+i = 0;
+while (i <= t)
+{
+if (i % 2 == 0)
 {
 _putchar(*str);
-str += 2;
+}
+i++;
+str++;
 }
 _putchar('\n');
 }
