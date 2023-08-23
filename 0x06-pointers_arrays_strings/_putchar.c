4b4653eaa1204dda5fea27dbd874f6100f09bd21
regragui nisrine <nisrineregragui10@gmail.com>
2023-08-23 03:57:54 -0700

    00

---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,9 @@
+#include "main.h"
+#include <unistd.h>
+/*b*/ int _putchar(char c) /*kk*/
+{
+return (write(1, &c, 1));
+}
+
+~                                                                                                                                       
+~                             
