8f643982dde0adabc82a7ee7342e43efe3ce0a9e
regragui nisrine <nisrineregragui10@gmail.com>
2023-08-24 11:28:19 -0700

    2

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,36 +1,32 @@
 #include "main.h"
-/*betty*/ char *cap_string(char *);/*style*/
-/*betty*/ char *cap_string(char *a)/*style*/
+/*betty*/ char *cap_string(char *); /*style*/
+/*betty*/ char *cap_string(char *) /*style*/
 {
-int i = 0, cp = 1;
-if (a == NULL)
+int i = 0;
+while (str[i])
 {
-return (NULL);
+while (str[i] >= 'a' && str[i] <= 'z')
+{
+i++;
+if (str[i - 1] == ' ' ||
+str[i - 1] == '\t' ||
+str[i - 1] == '\n' ||
+str[i - 1] == ';' ||
+str[i - 1] == ',' ||
+str[i - 1] == '.' ||
+str[i - 1] == '!' ||
+str[i - 1] == '?' ||
+str[i - 1] == '"' ||
+str[i - 1] == '(' ||
+str[i - 1] == ')' ||
+str[i - 1] == '{' ||
+str[i - 1] == '}' ||
+i == 0)
+{
+str[i] -= 32;
 }
-while (a[i] != '\0')
-{
-if ((a[i] >= 'a' && a[i] <= 'z') || (a[i] >= 'A' && a[i] <= 'Z'))
-{
-if (cp)
-{
-if (a[i] >= 'a' && a[i] <= 'z')
-{
-a[i] = a[i] - ('a' - 'A');
-}
-cp = 0;
-}
-else if (a[i] == ' ' || a[i] == '\t' || a[i] == '\n' || a[i] == ',' || a[i] == ';'
-|| a[i] == '.' || a[i] == '!' || a[i] == '?' || a[i] == '"' ||
-a[i] == '(' || a[i] == ')' || a[i] == '{' || a[i] == '}')
-{
-cp = 1;
-}
-}
-else
-{
-cp = 0;
 }
 i++;
 }
-return (a);
+return (str);
 }
