0769557c2745588686f8749f09bb8215eeeb8523
regragui nisrine <nisrineregragui10@gmail.com>
2023-08-24 12:03:24 -0700

    m

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/*betty*/ void print_number(int n); /*style*/
+/*betty*/ void print_number(int n) /*style*/
+{
+unsigned int n1;
+
+	n1 = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n1 = -n;
+	}
+
+	if (n1 / 10 != 0)
+	{
+		print_number(n1 / 10);
+	}
+	_putchar((n1 % 10) + '0');
+}
+
