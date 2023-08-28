fdda0ef6059b4158fac023d64050b3d3854b2dfa
regragui nisrine <nisrineregragui10@gmail.com>
2023-08-28 05:14:54 -0700

    87

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -7,20 +7,16 @@ f = 0;
 while (s[i] != '\0' && f == 0)
 {
 a = s[i];
-if (accept[i] == a)
-{
-f = 1;
-}
 for (k = 0; accept[k] != '\0'; k++)
 {
 if (accept[k] == a)
 {
 f = 1;
-break;
 }
 }
 i++;
 }
-return (i);
+return (k + 1);
+
 }
 
