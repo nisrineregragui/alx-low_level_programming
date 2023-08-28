29facacfba68b3bf6bd17e99364bc82aaba534dc
regragui nisrine <nisrineregragui10@gmail.com>
2023-08-28 02:49:56 -0700

    01

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -2,11 +2,13 @@
 /*betty*/ char *_memcpy(char *dest, char *src, unsigned int n) /*style*/
 {
 unsigned int i;
+char *p;
+p = dest;
 for (i = 0; i < n; i++)
 {
 *dest = *src;
 dest++;
 src++;
 }
-return (dest);
+return (p);
 }
