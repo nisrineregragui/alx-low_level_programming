a5452d1aaf8ffddff82e09a64e663a72b05a7c8e
regragui nisrine <nisrineregragui10@gmail.com>
2023-08-30 14:54:56 -0700

    05

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,14 +1,4 @@
 #include "main.h"
-/*betty*/ int _sqrt_recursion(int n) /*k*/
-{
-if (n < 0)
-{
-return (-1);
-}
-else
-{
-return (_sqrt_recursion1(n, 0));
-}
 /*s*/int _sqrt_recursion1(int n, int i)/*s*/
 {
 if (i * i > n)
@@ -20,4 +10,15 @@ if (i * i == n)
 return (i);
 }
 return (_sqrt_recursion1(n, i + 1));
+} 
+/*betty*/ int _sqrt_recursion(int n) /*k*/
+{
+if (n < 0)
+{
+return (-1);
+}
+else
+{
+return (_sqrt_recursion1(n, 0));
+}
 }
