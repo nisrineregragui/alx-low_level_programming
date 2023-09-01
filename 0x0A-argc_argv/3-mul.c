ac3e1997d8dece434f0aab8ad53a83cb68e35902
nisrineregragui <nisrineregragui10@gmail.com>
2023-09-01 11:27:18 -0700

    02

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "main.h"
+/*betty*/ int main(int argc, char *argv[]) /*style*/
+{
+int a, b;
+if (argc != 3)
+{
+printf("Error\n");
+return (1);
+}
+a = atoi(argv[1]);
+b = atoi(argv[2]);
+printf("%d\n", a * b);
+return (0);
+}
