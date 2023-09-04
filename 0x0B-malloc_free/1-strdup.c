3b80bdeebf47db1e70f4c84e369114c4caa7e522
nisrineregragui <nisrineregragui10@gmail.com>
2023-09-04 05:06:27 -0700

    task 01

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+/*comment*/char *_strdup(char *str) /*style*/
+{
+char *duplicate = (char *)malloc((strlen(str) + 1) * sizeof(char));
+if (str == NULL)
+{
+return (NULL);
+}
+strcpy(duplicate, str);
+if (duplicate == NULL)
+{
+return (NULL);
+}
+return (duplicate);
+}
+
