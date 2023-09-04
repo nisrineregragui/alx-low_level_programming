6644de78d3fa85924a714b165c85b4bdf2ef27ba
nisrineregragui <nisrineregragui10@gmail.com>
2023-09-04 07:36:58 -0700

    m

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -4,31 +4,21 @@
 /*comment*/ char *str_concat(char *s1, char *s2) /*comment*/
 {
 char *concat = (char *)malloc(((strlen(s1)) + strlen(s2) + 1));
-char *empty = (char *)malloc(1);
-if (empty == NULL)
-{
-return (NULL);
-}
-else
-{
-empty[0] = '\0';
-return (empty);
-}
 if (concat == NULL)
 {
 return (NULL);
 }
-if (s1 != NULL)
+if(s1 == NULL)
+{
+strcpy(concat, s2);
+return (concat);
+}
+else if (s2 == NULL)
 {
 strcpy(concat, s1);
+return (concat);
 }
-else
-{
-concat[0] = '\0';
-}
-if (s2 != NULL)
-{
+strcpy(concat, s1);
 strcat(concat, s2);
-}
 return (concat);
 }
