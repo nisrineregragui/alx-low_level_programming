4b82e2e4ef72049e906bb369688492260bb54edf
nisrineregragui <nisrineregragui10@gmail.com>
2023-09-13 08:31:13 -0700

    01

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+/*m*/void array_iterator(int *array, size_t size, void (*action)(int))/*mm*/
+{
+unsigned int i;
+if (array == NULL || action == NULL)
+{
+return;
+}
+for(i = 0; i < size; i++)
+{
+action(array[i]);
+}
+}
