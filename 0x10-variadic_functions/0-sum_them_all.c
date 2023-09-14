4b45b8770991a9c0a2c98728ef339e02fcc1d532
nisrineregragui <nisrineregragui10@gmail.com>
2023-09-14 08:49:39 -0700

    m

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,14 +1,16 @@
+#include <stdarg.h>
 #include "variadic_functions.h"
 /*betty*/int sum_them_all(const unsigned int n, ...)/*s*/
 {
-int s = 0, i = 0;
+unsigned int s = 0, i = 0;
 va_list args;
 va_start(args, n);
-if (n == NULL)
+if (n == 0)
 {
+va_end(args);
 return (0);
 }
-for (i = 0; i < args; i++)
+for (i = 0; i < n; i++)
 {
 s += va_arg(args, const unsigned int);
 }
