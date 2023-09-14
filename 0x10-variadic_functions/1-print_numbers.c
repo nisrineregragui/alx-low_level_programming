d6b963020718353404f5fca16961affe262e86ff
nisrineregragui <nisrineregragui10@gmail.com>
2023-09-14 09:10:45 -0700

    m

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,26 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+/*betty*/void print_numbers(const char *separator, const unsigned int n, ...)/*style*/
+{
+va_list args;
+unsigned int i;
+va_start(args, n);
+if (separator == NULL)
+{
+for(i = 0; i < n; i++)
+{
+printf("%d", va_arg(args, int));
+}
+printf("\n");
+return;
+}
+for(i = 0; i < n; i++)
+{
+printf("%d", va_arg(args, int));
+if(i < (n - 1))
+{
+printf("%s", separator);
+}
+}
+printf("\n");
+}
