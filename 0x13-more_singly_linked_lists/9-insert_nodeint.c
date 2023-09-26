9c0407207eec8202bb3d26816ab5772325bfb9f6
nisrineregragui <nisrineregragui10@gmail.com>
2023-09-26 14:41:41 -0700

    m

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,5 +1,13 @@
 #include "lists.h"
-/*g*/listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)/*l*/
+/**
+ * delete_nodeint_at_index - deletes a node in a linked list at a certain index
+ * @head: pointer to the first element in the list
+ * @index: index of the node to delete
+ *
+ * Return: 1 (Success), or -1 (Fail)
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 listint_t *temp = *head, *new;
 unsigned int i;
@@ -29,5 +37,5 @@ else
 temp = temp->next;
 }
 }
-return(NULL);
+return (NULL);
 }
