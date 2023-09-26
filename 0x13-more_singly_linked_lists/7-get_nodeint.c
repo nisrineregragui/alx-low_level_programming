e7d2ca385ef31c0760987fc6f2096d056541b457
nisrineregragui <nisrineregragui10@gmail.com>
2023-09-26 14:00:55 -0700

    06

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+/*b*/ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index) /*s*/
+{
+listint_t *tmp = head;
+unsigned int i;
+i = 0;
+while (tmp && i < index)
+{
+tmp = tmp->next;
+i++;
+}
+return (tmp ? tmp : NULL);
+}
+
