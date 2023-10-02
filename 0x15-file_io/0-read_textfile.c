78a9b07e8bec1b2ae5d80bc2f8bc05a978064722
nisrineregragui <nisrineregragui10@gmail.com>
2023-10-02 13:39:42 -0700

    00

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/*n*/int create_file(const char *filename, char *text_content)/*m*/
+{
+int f, s, l = 0;
+
+if (filename == NULL)
+{
+	return (-1);
+}
+	if (text_content != NULL)
+	{
+		for (l = 0; text_content[l];)
+			l++;
+	}
+
+	f = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	s = write(fd, text_content, l);
+
+	if (f == -1 || s == -1)
+{
+	return (-1);
+}
+	close(f);
+
+	return (1);
+}
+
